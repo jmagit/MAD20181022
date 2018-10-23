1cae0d81d7f83b20b436e86d1382de7c8bda5ab3
Javier <jmartin@grupoloyal.com>
2018-10-23 15:43:27 +0200

    directivas

---
+++ src/indra-core/pipes/cadenas.pipe.ts
@@ -0,0 +1,10 @@
+import { Pipe, PipeTransform } from '@angular/core';
+@Pipe({
+  name: 'elipsis'
+})
+export class ElipsisPipe implements PipeTransform {
+  transform(value: any, maxlen: number): any {
+    return (!maxlen || !value || value.length < maxlen || value.length < 4)
+      ? value : (value.substr(0, maxlen - 3) + '...');
+  }
+}
